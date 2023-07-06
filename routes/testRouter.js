877a86925d154b24a36899bb4af5bfb21eb940ce
vav <vavvav@i.ua>
2023-07-06 22:47:40 +0300

    dstatus

--- routes/testRouter.js
+++ routes/testRouter.js
@@ -4,7 +4,7 @@ const router = express.Router()
  
 // define the home page route
 router.get('/', (req, res) => {
-  res.send('test homepage')
+  res.send('test  ---')
 })
  
 //module.exports = router
